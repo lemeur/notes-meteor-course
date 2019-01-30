50643e619f67b322c85e0687f7cc8e562e235ef8
Thibault Le Meur <lemeur@MacBook-Pro-de-Thibault.local>
2019-01-30 23:36:54 +0100

    Testing component with spy passed With a createContainer component

--- imports/ui/PrivateHeader.test.js
+++ imports/ui/PrivateHeader.test.js
@@ -3,23 +3,35 @@ import React from 'react';
 import expect from 'expect';
 import { mount } from 'enzyme';
 
-import PrivateHeader from './PrivateHeader';
+import {PrivateHeader} from './PrivateHeader';
 
 if (Meteor.isClient) {
   describe('PrivateHeader', function() {
     it('should set button text to logout', function() {
       const testTitle= "testTitle";
-      const wrapper = mount(<PrivateHeader title={testTitle}/>);
+      const wrapper = mount(<PrivateHeader title={testTitle} handleLogout={ () => {} }/>);
       const buttonText = wrapper.find('button').text();
       expect(buttonText).toBe('Logout');
     });
 
     it('should set Title the prop title value', function() {
       const testTitle= "testTitle";
-      const wrapper = mount(<PrivateHeader title={testTitle}/>);
+      const wrapper = mount(<PrivateHeader title={testTitle} handleLogout={ () => {} }/>);
       const buttonText = wrapper.find('h1').text();
       expect(buttonText).toBe(testTitle);
     });
+
+    it('should call Account.Logout on click', function() {
+      const spy = expect.createSpy();
+
+      const testTitle= "testTitle";
+      const wrapper = mount(<PrivateHeader title={testTitle} handleLogout={spy}/>);
+
+      wrapper.find('button').simulate('click');
+      expect(spy).toHaveBeenCalled();
+
+    });
+
 
   }); // End describe
 }
