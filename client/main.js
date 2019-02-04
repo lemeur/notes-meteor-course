97cb193add92f4416074418a8d2e5cfe2173e3b0
Thibault Le Meur <lemeur@MacBook-Pro-de-Thibault.local>
2019-02-04 22:26:46 +0100

    Add the Session selectedNoteId, a tracker to updated URL, and a onClick handler in NoteListItem

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import ReactDom from 'react-dom';
 import {Tracker} from 'meteor/tracker';
 import {Session} from 'meteor/session';
+import { browserHistory } from 'react-router';
 
 import {routes, onAuthChange} from './../imports/routes/routes';
 import '../imports/startup/simple-schema-configuration.js';
@@ -12,7 +13,17 @@ Tracker.autorun( () => {
   onAuthChange(isAuthenticated);
 });
 
+Tracker.autorun( () => {
+  // When selectedNoteId changes, update the URL
+  const selectedNoteId = Session.get('selectedNoteId');
+
+  if (selectedNoteId) {
+    browserHistory.replace(`/dashboard/${selectedNoteId}`);
+  }
+});
+
 
 Meteor.startup(() => {
+  Session.set('selectedNoteId', undefined);
   ReactDom.render(routes,document.getElementById('app'));
 });
--- imports/ui/NoteListItem.js
+++ imports/ui/NoteListItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import moment from 'moment';
+import {Session} from 'meteor/session';
+import {createContainer} from 'meteor/react-meteor-data';
+
 import PropTypes from 'prop-types';
 
-const NoteListItem =  (props) => {
+export const NoteListItem =  (props) => {
   return (
-    <div>
+    <div onClick={() => {props.Session.set('selectedNoteId', props.note._id)} } >
       <h5>{props.note.title || 'Untitled note'}</h5>
       <p>{moment(props.note.updatedAt).format('DD/MM/YYYY')}</p>
     </div>
@@ -13,7 +15,12 @@ const NoteListItem =  (props) => {
 }
 
 NoteListItem.propTypes = {
-  note: PropTypes.object.isRequired
+  note: PropTypes.object.isRequired,
+  Session: PropTypes.object.isRequired
 };
 
-export default  NoteListItem;
+export default createContainer(() => {
+  return {
+    Session
+  }
+},  NoteListItem);
